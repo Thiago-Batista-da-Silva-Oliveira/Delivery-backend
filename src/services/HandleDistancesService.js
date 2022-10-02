7f45287a3c13421e761ccd7805609ac9f0ca9d59
Thiago Oliveira <77520434+Thiago-Batista-da-Silva-Oliveira@users.noreply.github.com>
2022-10-02 16:07:39 -0300

    Update HandleDistancesService.js

--- src/services/HandleDistancesService.js
+++ src/services/HandleDistancesService.js
@@ -8,20 +8,6 @@ class HandleDistancesService{
 
 const REACT_APP_GOOGLE_API_KEY = 
 
-
-
-let time;
-
-let time1 = null
-let time2 = null
-let time3 = null
-let time4 = null
-
-let delivery1 = 'asdasd'
-let delivery2 = 'sadasf';
-let delivery3 = 'afsafa';
-let delivery4 = 'dasfsa';
-
  let origin = pointA.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
  //let origin = 'Rua Felipe Cardoso'
 //let dest = ['Rua Nestor','Rua Felipe Cardoso','Rua General Olimpio']
@@ -110,97 +96,6 @@ for(let i =0; i<totalLength; i++) {
     prioritiesDest.splice(index,1)
   }
   
-
- /*
-  if(lastPriority && i == destLength - 1) {
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${lastPriority}&key=${REACT_APP_GOOGLE_API_KEY}`)
-    results.push({time:response.data.rows[0]?.elements[0]?.duration?.value,
-      destination: response.data.destination_addresses[0]
-     })
-     delivery = response.data.destination_addresses[0]
-  }
-  */
-}
-
-
-/*
-for(let i =0; i<destLength; i++) {
-  if(priority) {
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${priority}&key=${REACT_APP_GOOGLE_API_KEY}`)
-    console.log(response)
-    results.push({time:response.data.rows[0]?.elements[0]?.duration?.value,
-      destination: response.data.destination_addresses[0]
-     })
-    delivery = response.data.destination_addresses[0]
-    origin = delivery.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-    priority = null
-  }
-  
-  if(!priority){
-    const destJoinend = dest.join('|').normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destJoinend}&key=${REACT_APP_GOOGLE_API_KEY}`)
-    for(let u=0 ; u<dest.length; u++){
-     
-     
-      if(!results[i] ) {
-        results.push({time:response.data.rows[0]?.elements[u]?.duration?.value,
-          destination: response.data.destination_addresses[u]
-         })
-         delivery = response.data.destination_addresses[u]
-      }
-        if(results[i]?.time > response.data.rows[0]?.elements[u]?.duration?.value ){
-            delivery = response.data.destination_addresses[u]
-            results[i].time = response.data.rows[0]?.elements[u]?.duration?.value
-            results[i].destination = response.data.destination_addresses[u]
-     }
-    }
-    const index =  response.data.destination_addresses.indexOf(delivery, 0)
-    origin = dest[index].normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-    dest.splice(index,1)
-  }
-
-  if(lastPriority && i == destLength - 1) {
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${lastPriority}&key=${REACT_APP_GOOGLE_API_KEY}`)
-    results.push({time:response.data.rows[0]?.elements[0]?.duration?.value,
-      destination: response.data.destination_addresses[0]
-     })
-     delivery = response.data.destination_addresses[0]
-  }
-}
-*/
-
-/*
-for(let i =0; i<destLength; i++) {
-  
-  const destJoinend = dest.join('|').normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-  const response = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destJoinend}&key=${REACT_APP_GOOGLE_API_KEY}`)
- 
-  for(let u=0 ; u<dest.length; u++){
-   
-   
-    if(!results[i] ) {
-      results.push({time:response.data.rows[0]?.elements[u]?.duration?.value,
-        destination: response.data.destination_addresses[u]
-       })
-       delivery = response.data.destination_addresses[u]
-    }
-      if(results[i]?.time > response.data.rows[0]?.elements[u]?.duration?.value ){
-          delivery = response.data.destination_addresses[u]
-          results[i].time = response.data.rows[0]?.elements[u]?.duration?.value
-          results[i].destination = response.data.destination_addresses[u]
-   }
-  }
-  const index =  response.data.destination_addresses.indexOf(delivery, 0)
-  origin = dest[index].normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-  dest.splice(index,1)
-}
-
-*/
-
-time = time1 + time2 + time3 + time4
-      // return {delivery1, delivery2, delivery3, delivery4,time1, time2, time3,time4, time}
- 
-     
      return results
   
   }
